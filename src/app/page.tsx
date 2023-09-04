1683c1495a73179f0a096ab024acfdd13d3eb70c
Vanessa Ferreira <vanessaf689@gmail.com>
2023-09-04 09:20:51 -0300

    feat: form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 
+import { InputControl, InputPrefix, InputRoot } from '@/components/Input'
 import { SetttingsTabs } from '@/components/SettingsTabs'
+import { Mail } from 'lucide-react'
+
+import * as FileInput from '@/components/Form/FileInput'
 
 export default function Home() {
   return (
@@ -8,6 +12,117 @@ export default function Home() {
         Settings
       </h1>
       <SetttingsTabs />
+      <div className='mt-6 flex flex-col'>
+        <div className='flex justify-between items-center border-b border-zinc-200 pb-5'>
+          <div className='space-y-1'>
+            <h2 className='text-lg font-medium text-zinc-900'>Personal Info</h2>
+            <span className='text-sm text-zinc-500'>Update your photo and personal details here.</span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <button
+              type='button'
+              className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border-zinc-300 text-zinc-700 hover:bg-zinc-50'
+            >
+              Cancel
+            </button>
+            <button
+              type='submit'
+              form='settings'
+              className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700'
+            >
+              Save
+            </button>
+          </div>
+        </div>
+        <form id="settings" className='mt-6 flex flex-col w-full gap-5 divide-y divide-zinc-200'>
+          <div className='grid gap-3 grid-cols-form'>
+            <label htmlFor='firstName' className='text-sm font-medium text-zinc-700'>Name</label>
+            <div className='grid gap-6 grid-cols-2'>
+              <InputRoot>
+                <InputControl id="firstName" defaultValue="Vanessinha" />
+              </InputRoot>
+              <InputRoot>
+                <InputControl defaultValue="Ferreira" />
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className='grid gap-3 grid-cols-form pt-5'>
+            <label htmlFor='emailAddress' className='text-sm font-medium text-zinc-700'>Email address</label>
+            <InputRoot>
+              <InputPrefix>
+                <Mail className='h-5 w-5 text-zinc-500' />
+              </InputPrefix>
+              <InputControl id="emailAddress" defaultValue="vanessaf689@gmail.com" />
+            </InputRoot>
+          </div>
+
+          <div className='grid gap-3 grid-cols-form pt-5'>
+            <label htmlFor='yourPhoto' className='text-sm font-medium text-zinc-700'>
+              Your photo
+              <span className='mt-0.5 text-sm font-normal text-zinc-500 block'>This will be displayed on your profile.</span>
+            </label>
+            <FileInput.Root className='flex items-start gap-5'>
+              <FileInput.ImagePreview />
+              <FileInput.Trigger />
+              <FileInput.Control />
+            </FileInput.Root>
+          </div>
+
+          <div className='grid gap-3 grid-cols-form pt-5'>
+            <label htmlFor='role' className='text-sm font-medium text-zinc-700'>Role</label>
+            <InputRoot>
+              <InputControl id="role" defaultValue="Product Designer" />
+            </InputRoot>
+          </div>
+
+          <div className='grid gap-3 grid-cols-form pt-5'>
+            <label htmlFor='country' className='text-sm font-medium text-zinc-700'>Country</label>
+            <div></div>
+          </div>
+
+          <div className='grid gap-3 grid-cols-form pt-5'>
+            <label htmlFor='timezone' className='text-sm font-medium text-zinc-700'>Timezone</label>
+            <div></div>
+          </div>
+
+          <div className='grid gap-3 grid-cols-form pt-5'>
+            <label htmlFor='bio' className='text-sm font-medium text-zinc-700'>
+              Bio
+              <span className='mt-0.5 text-sm font-normal text-zinc-500 block'>Write a short introduction.</span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className='grid gap-3 grid-cols-form pt-5'>
+            <label htmlFor='portfolioProjects' className='text-sm font-medium text-zinc-700'>
+              Portfolio projects
+              <span className='mt-0.5 text-sm font-normal text-zinc-500 block'>Share a few snippets of your work.</span>
+            </label>
+            <FileInput.Root className='gap-5'>
+              <FileInput.Trigger />
+              <FileInput.FileList />
+              <FileInput.Control multiple/>
+            </FileInput.Root>
+            <div></div>
+          </div>
+
+          <div className='flex justify-end items-center gap-2 pt-5'>
+            <button
+              type='button'
+              className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border-zinc-300 text-zinc-700 hover:bg-zinc-50'
+            >
+              Cancel
+            </button>
+            <button
+              type='submit'
+              className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700'
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
---
+++ src/components/Form/FileInput/Root.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { ComponentProps, createContext, useContext, useId, useState } from "react"
+
+type FileInputContextType = {
+  id: string
+  files: File[]
+  onFilesSelected: (file: File[], multiple: boolean) => void
+}
+
+const FileInputContext = createContext({} as FileInputContextType)
+
+export type RootProps = ComponentProps<'div'>
+
+export function Root(props: RootProps) {
+  const id = useId() // TODO: cria um id unico que é persistido entre as renderizações
+  const [files, setFiles] = useState<File[]>([])
+
+  function onFilesSelected(files: File[], multiple: boolean) {
+    if (multiple) {
+      setFiles((state) => [...state, ...files])
+    } else {
+      setFiles(files)
+    }
+  }
+
+  return (
+    <FileInputContext.Provider value={{ id, files, onFilesSelected }}>
+      <div {...props}/>
+    </FileInputContext.Provider>
+  )
+}
+
+export const useFileInput = () => useContext(FileInputContext)
