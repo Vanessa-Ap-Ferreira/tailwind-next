c01e3e756d2fee09e13b6b5075ffd1668fa399be
Vanessa Ferreira <vanessaf689@gmail.com>
2023-09-12 09:40:58 -0300

    fix: ajustes responsividade sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,7 +7,7 @@ import { UsedSpaceWidget } from "./UsedSpaceWidget";
 
 export function Sidebar() {
   return (
-    <aside className='flex flex-col gap-6 p-4 border-b border-zinc-200 fixed left-0 top-0 right-0 bottom-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative'>
+    <aside className='flex flex-col gap-6 p-4 border-b border-zinc-200 fixed left-0 top-0 right-0 bottom-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 h-screen'>
       <Logo />
       <InputRoot>
         <InputPrefix>
